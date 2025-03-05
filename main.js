075935e0d7bd9b71841d81a9f530b63b9713977c
MarcoVacchi <mvacchi816@gmail.com>
2025-03-05 16:24:30 +0100

    feat: create first structure html/css

--- main.js
+++ main.js
@@ -69,13 +69,16 @@ formElement.addEventListener('submit', function(event) {
     }
 })
 
-/*
-MILESTONE 2:
-Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form
- in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
-Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina
- (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo). 
-Questo richiederà un minimo di ricerca.*/
+/*MILESTONE 3:
+Ora che la logica è funzionante in pagina, possiamo andare a dedicarci allo stile, raffinando
+ la parte di HTML e CSS in modo da renderla esteticamente gradevole.
+Nota:
+Se non vi sentite particolarmente creativi, questa potrebbe essere un’implementazione da 
+seguire per il secondo milestone.
+Potete scegliere di implementare una soluzione completamente diversa oppure simile, ma 
+in ogni caso cercate di farla vostra.
+Buona giornata, buon lavoro :saluto_vulcaniano: */
+
 
 
 
