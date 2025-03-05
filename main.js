bfaa7e3c2605006bedd2e1fc55f4741274866072
MarcoVacchi <mvacchi816@gmail.com>
2025-03-05 15:06:29 +0100

    feat: complete milestone 2

--- main.js
+++ main.js
@@ -38,36 +38,46 @@ Buona giornata, buon lavoro :saluto_vulcaniano: */
 const kmElement = document.getElementById("km");
 const ageElement = document.getElementById("age-user");
 const btnElement = document.getElementById("output");
-//const containerElement = document.querySelector("container-primary");
+const formElement = document.getElementById("my-form");
 
-btnElement.addEventListener('click', function() {
-let kmElementValue = parseInt(kmElement.value);
-let ageElementValue = parseInt(ageElement.value);
-const priceForKm = 0.21;
-let totalPrice = kmElementValue * priceForKm;
+formElement.addEventListener('submit', function(event) {
+    event.preventDefault();
+    let kmElementValue = parseInt(kmElement.value);
+    let ageElementValue = parseInt(ageElement.value);
+    let priceForKm = 0.21;
+    let totalPrice = kmElementValue * priceForKm;
+    //let totalPriceForUser = document.getElementById("total-price-for-user")
 
+    // Verifica se kmElementValue non sia un numero
 
-// Verifica se kmElementValue non sia un numero
+    if (isNaN(kmElementValue) || kmElementValue === '' || kmElementValue <= 0 ) {
+        alert( 'Please, insert a valid number for the km, not a word, negative number or a space!' );
+    } 
 
-if (isNaN(kmElementValue) || kmElementValue === '' || kmElementValue <= 0 ) {
-    alert( 'Please, insert a valid number for the km, not a word, negative number or a space!' );
-} 
-
-    // Verifica se ageElementValue non è un numero
-if (isNaN(ageElementValue) || ageElementValue === '' || ageElementValue <= 0 ) {
-    alert( 'Please, insert a valid number for the age, not a word, negative number or a space!' );
-    } else {
-        // Controlla l'età e applica lo sconto
-        if (ageElementValue <= 18) {
-            totalPrice = totalPrice - (totalPrice * 20) / 100; // applico lo sconto del 20% per -18
-        } else if (ageElementValue >= 65) {
-            totalPrice = totalPrice - (totalPrice * 40) / 100; // applico lo sconto del 40% per gli over 65
-        }
-        // applico il prezzo finale in forma umana con massimo 2 decimali (per indicare i centesimi)
-        console.log("Great! The price is: €" + totalPrice.toFixed(2));
+        // Verifica se ageElementValue non è un numero
+    if (isNaN(ageElementValue) || ageElementValue === '' || ageElementValue <= 0 ) {
+        alert( 'Please, insert a valid number for the age, not a word, negative number or a space!' );
+        } else {
+            // Controlla l'età e applica lo sconto
+            if (ageElementValue <= 18) {
+                totalPrice = totalPrice - (totalPrice * 20) / 100; // applico lo sconto del 20% per -18
+            } else if (ageElementValue >= 65) {
+                totalPrice = totalPrice - (totalPrice * 40) / 100; // applico lo sconto del 40% per gli over 65
+            }
+            // applico il prezzo finale in forma umana con massimo 2 decimali (per indicare i centesimi)
+            totalPriceForUser = document.getElementById("total-price-for-user").innerHTML = "Great! The price is: €" + totalPrice.toFixed(2);
     }
-
 })
+
+/*
+MILESTONE 2:
+Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form
+ in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
+Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina
+ (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo). 
+Questo richiederà un minimo di ricerca.*/
+
+
 
 
 
